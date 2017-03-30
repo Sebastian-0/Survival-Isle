6e9a24f8ebd493bc4e42c1d94257714ef4cd269c
Sebastian-0 <dat13shj@student.lu.se>
2017-03-30 15:57:20 +0200

    Created gradle project

---
+++ Survival-Isle/desktop/src/isle/survival/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package isle.survival.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import isle.survival.SurvivalIsle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SurvivalIsle(), config);
+	}
+}
---
+++ Survival-Isle/html/src/isle/survival/client/HtmlLauncher.java
@@ -0,0 +1,20 @@
+package isle.survival.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+import isle.survival.SurvivalIsle;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SurvivalIsle();
+        }
+}
