b3a22dcafea38e3325411e9ff579558b9edfd8cc
Mattias Gustafsson <dat13mgu@student.lu.se>
2017-04-06 14:40:33 +0200

    Moved textures to texture base and started to use it

--- Survival-Isle/core/src/isle/survival/world/ClientWorld.java
+++ Survival-Isle/core/src/isle/survival/world/ClientWorld.java
@@ -1,36 +1,23 @@
 package isle.survival.world;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import world.World;
 
 public class ClientWorld extends World {
-	private Texture[] textures;
+	private TextureBase textureBase;
 	
-	public ClientWorld(int width, int height) {
+	public ClientWorld(int width, int height, TextureBase textureBase) {
 		super(width, height);
-		setUpTextures();
-	}
-
-	private void setUpTextures() {
-		textures = new Texture[GroundTile.values().length];
-		for (GroundTile t : GroundTile.values()) {
-			textures[t.id] = new Texture(t.textureName);
-		}
+		this.textureBase = textureBase;
 	}
 
 	public void drawTerrain(SpriteBatch spriteBatch) {
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
-				spriteBatch.draw(textures[ground[i][j]], i*TILE_WIDTH, j*TILE_HEIGHT);
+				spriteBatch.draw(textureBase.getGroundTexture(ground[i][j]), i*TILE_WIDTH, j*TILE_HEIGHT);
 			}
 		}
 	}
-
-	public void dispose() {
-		for (Texture t : textures) {
-			t.dispose();
-		}
-	}
+	
 }
