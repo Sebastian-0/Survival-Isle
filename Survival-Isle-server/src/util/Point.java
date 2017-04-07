f8ea1ee234d72908ff53536aea99038fda632e8f
Sebastian-0 <dat13shj@student.lu.se>
2017-04-07 14:08:14 +0200

    Added initial version of interpolation for player movement.

--- Survival-Isle-server/src/util/Point.java
+++ Survival-Isle-server/src/util/Point.java
@@ -13,8 +13,25 @@ public class Point {
 		this.y = y;
 	}
 	
+	public void set(Point other) {
+		set(other.x, other.y);
+	}
+	
+	public void set(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 	@Override
 	public String toString() {
 		return String.format("(%d, %d)", (int) x, (int) y);
+	}
+
+	public Point interpolateTo(Point target, float i) {
+		if (i == 0)
+			return this;
+		if (i == 1)
+			return target;
+		return new Point(x * (1 - i) + i * target.x, y * (1 - i) + i * target.y);
 	}
 }
--- Survival-Isle/core/src/isle/survival/world/NetworkObject.java
+++ Survival-Isle/core/src/isle/survival/world/NetworkObject.java
@@ -1,24 +1,35 @@
 package isle.survival.world;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import util.Point;
 import world.World;
 
 public class NetworkObject {
+	private static final float MOVEMENT_TIME = 0.5f;
+	
 	private int id;
-	private float x;
-	private float y;
 	private int textureId;
 	
+	private Point previousPosition;
+	private Point position;
+	private float interpolation;
+	
 	public NetworkObject(int x, int y, int id, int textureId) {
-		this.x = x;
-		this.y = y;
+		position = new Point(x, y);
+		previousPosition = new Point(x, y);
 		this.id = id;
 		this.textureId = textureId;
 	}
 	
 	public void draw(SpriteBatch spriteBatch, TextureBase textures, float xView, float yView) {
-		spriteBatch.draw(textures.getObjectTexture(textureId),x*World.TILE_WIDTH - xView, y*World.TILE_HEIGHT - yView);
+		float dt = Gdx.graphics.getDeltaTime();
+		interpolation = Math.min(1, interpolation + dt/MOVEMENT_TIME);
+		Point currentPosition = previousPosition.interpolateTo(position, interpolation);
+		spriteBatch.draw(textures.getObjectTexture(textureId), 
+				currentPosition.x*World.TILE_WIDTH - xView,
+				currentPosition.y*World.TILE_HEIGHT - yView);
 	}
 	
 
@@ -27,15 +38,16 @@ public class NetworkObject {
 	}
 
 	public float getX() {
-		return x;
+		return position.x;
 	}
 
 	public float getY() {
-		return y;
+		return position.y;
 	}
 
 	public void setPosition(int x, int y) {
-		this.x = x;
-		this.y = y;
+		previousPosition.set(previousPosition.interpolateTo(position, interpolation));
+		position.set(x, y);
+		interpolation = 0;
 	}
 }
