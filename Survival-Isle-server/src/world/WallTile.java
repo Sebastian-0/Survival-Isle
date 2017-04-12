ec64c4a9c2740e9285260effaee317a44d51bcab
Mattias-G <dat13mgu@student.lu.se>
2017-04-12 11:25:51 +0200

    Changed wall tile constructors

--- Survival-Isle-server/src/world/WallTile.java
+++ Survival-Isle-server/src/world/WallTile.java
@@ -2,24 +2,33 @@ package world;
 
 public class WallTile {
 	private int id;
+	private boolean isBreakable;
 	
 	public enum TileType {
-		Forest(0);
+		Forest(0, true);
 		
-		
+
 		public final int id;
+		public final boolean isBreakable;
 		
-		private TileType(int id) {
+		private TileType(int id, boolean isBreakable) {
 			this.id = id;
+			this.isBreakable = isBreakable;
 		}
 	}
 	
-	public WallTile(int id) {
-		this.id = id;
+	public WallTile(TileType type) {
+		id = type.id;
+		isBreakable = type.isBreakable;
 	}
 	
 	
 	public int getId() {
 		return id;
+	}
+	
+	
+	public boolean isBreakable() {
+		return isBreakable;
 	}
 }
