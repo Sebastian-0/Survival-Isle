0a4741362ace2c95e617b6fb021c0618fc204ae8
Sebastian Hjelm <melon.bag@hotmail.com>
2017-04-14 12:49:00 +0200

    Made the server start when launching a client.

--- Survival-Isle/desktop/src/isle/survival/desktop/DesktopLauncher.java
+++ Survival-Isle/desktop/src/isle/survival/desktop/DesktopLauncher.java
@@ -1,19 +1,20 @@
 package isle.survival.desktop;
 
+import isle.survival.client.SurvivalIsleClient;
+
 import java.io.IOException;
 
 import javax.swing.JOptionPane;
 
+import server.Server;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import isle.survival.client.SurvivalIsleClient;
-import server.Server;
-
 public class DesktopLauncher {
 	public static void main (String[] arg) throws IOException {
-//		Server server = new Server();
-//		server.start();
+		Server server = new Server();
+		server.start();
 		
 		String name = JOptionPane.showInputDialog("Enter name");
 		if (name != null) {
