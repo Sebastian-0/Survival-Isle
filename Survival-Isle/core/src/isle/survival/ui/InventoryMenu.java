cf4f67000155c6a0c364d9b5d33b7ceb0b29436f
Sebastian-0 <dat13shj@student.lu.se>
2017-05-04 22:25:21 +0200

    Changed the color of the inventory font.

--- Survival-Isle/core/src/isle/survival/ui/InventoryMenu.java
+++ Survival-Isle/core/src/isle/survival/ui/InventoryMenu.java
@@ -5,6 +5,7 @@ import world.ItemType;
 import isle.survival.world.TextureBase;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
@@ -20,6 +21,7 @@ public class InventoryMenu {
 		items = new Array<>();
 		
 		font = new BitmapFont(Gdx.files.internal("font32.fnt"));
+		font.setColor(new Color(235/255f, 240/255f, 240/255f, 1));
 		items.add(new InventoryItem(textures.getTexture("wood_icon"), "Wood: ", font, inventory, ItemType.Wood));
 		items.add(new InventoryItem(textures.getTexture("stone_icon"), "Stone: ", font, inventory, ItemType.Stone));
 		items.add(new InventoryItem(textures.getTexture("respawn_crystal_icon"), "Respawn crystal: ", font, inventory, ItemType.RespawnCrystal));
